a165b140e00d2d7f74670523dabf0a04fc24bcf9
mahi97 <mohammadmahdi76@gmail.com>
2017-01-18 00:54:10 +0330

    fix traffic controller

--- Logic_fall2016/final_Project/traffic_controller.v
+++ Logic_fall2016/final_Project/traffic_controller.v
@@ -4,10 +4,16 @@
 `include "noTraffic.v"
 `include "bcd_counter.v"
 
-module trafficLightControlSystem (A_Time_L,A_Time_H,B_Time_L,B_Time_H,A_Light,B_Light,CLK,R,A,B,A_Traffic,B_Traffic);
-  input CLK,R,A,B,A_Traffic,B_Traffic;
-  output [3:0] A_Time_L,A_Time_H,B_Time_L,B_Time_H;
-  output A_Light,B_Light;
+module trafficLightControlSystem (A_Time_L , A_Time_H ,
+                                  B_Time_L , B_Time_H ,
+                                  A_Light  , B_Light  ,
+                                  CLK      , R        ,
+                                  A        , B        ,
+                                  A_Traffic, B_Traffic);
+
+  input CLK, R, A, B, A_Traffic, B_Traffic;
+  output [3:0] A_Time_L, A_Time_H, B_Time_L, B_Time_H;
+  output A_Light, B_Light;
 
   wire timeout;
   wire [3:0] lmsb;
@@ -17,10 +23,10 @@ module trafficLightControlSystem (A_Time_L,A_Time_H,B_Time_L,B_Time_H,A_Light,B_
   wire manual;
   wire A_A, A_B;
   wire M_A, M_B;
-  auto automodule (R,CLK,timeout,lmsb,llsb,ltrg, A_A, A_B);
-  man  manlmodule (A, B, R, CLK, M_A, M_B, manual);
-  out  outmodule  (manual, CLK, A_A, A_B, M_A, M_B, A_Light, B_Light);
-  noTraffic notrf(timeout, A_Light, B_Light, A_Traffic, B_Traffic, CLK);
+  auto      automodule (R, CLK, timeout, lmsb, llsb, ltrg, A_A, A_B);
+  man       manlmodule (A, B, R, CLK, M_A, M_B, manual);
+  out       outmodule  (manual, CLK, A_A, A_B, M_A, M_B, A_Light, B_Light);
+  noTraffic notrf      (timeout, A_Light, B_Light, A_Traffic, B_Traffic, CLK);
 
   wire [3:0] bcdmsb;
   wire [3:0] bcdlsb;
@@ -31,8 +37,8 @@ module trafficLightControlSystem (A_Time_L,A_Time_H,B_Time_L,B_Time_H,A_Light,B_
   wire lcount;
   assign lcount = 1;
   wire luf,muf;
-  bcdCntr lsbbcdmodule (bcdlsb,lcount,luf,ltrg,llsb,CLK);
-  bcdCntr msbbcdmodule (bcdmsb,luf,muf,ltrg,lmsb,CLK);
+  bcdCntr lsbbcdmodule (bcdlsb, lcount, luf, ltrg, llsb, CLK);
+  bcdCntr msbbcdmodule (bcdmsb, luf   , muf, ltrg, lmsb, CLK);
   
   reg cntto;
   assign timeout = cntto; /////////temporary
@@ -42,6 +48,5 @@ module trafficLightControlSystem (A_Time_L,A_Time_H,B_Time_L,B_Time_H,A_Light,B_
       assign cntto=1;
     else
       assign cntto=0;
-
 
 endmodule
