262ed39de1967e7f41ec3b2fd5a9f9d012d65ec4
mahi97 <mohammadmahdi76@gmail.com>
2017-04-21 23:32:25 +0430

    final controller

--- CA_spring2017/MID-Project/Controller.vhd
+++ CA_spring2017/MID-Project/Controller.vhd
@@ -29,7 +29,7 @@ entity Controller is
 end entity ; -- Controller
 
 architecture RTL of Controller is
-	type state is (S0, S1, S2, S3, S4, S5, S6, SH);
+	type state is (S0, S1, S2, S3, S35, S4, SW, S5, SH);
 	signal current_state : state;
 	signal next_state : state;
 begin
@@ -47,7 +47,7 @@ begin
 	process (current_state)
 	begin
 		case current_state is
-			when S0 =>
+			when S0 => 
 				readMem <= '1';
 				writeMem <= '0';
 				ResetPC <= '1';
@@ -74,7 +74,7 @@ begin
 				RFDWrite <= '0';
 
 				next_state <= S1;
-			when S1 =>
+			when S1 => 
 				readMem <= '0';
 				IRLoad <= '1';
 				ResetPC <= '0';
@@ -82,8 +82,10 @@ begin
 				CReset <= '0';
 				ZReset <= '0';
 				WPReset <= '0';
+				PCplus1 <= '0';
+
 				next_state <= S2;
-			when S2 =>
+			when S2 => 
 				IRLoad <= '0';
 				data <= IRout(15 downto 8);
 				if IRout(15 downto 12) = "1111" 
@@ -92,10 +94,11 @@ begin
 					shadow <= '1';
 				end if;
 				next_state <= S3;
-			when S3 =>
+			when S3 => 
 				if IRout(15 downto 12) = "0000" then
+					next_state <= SW;
 					case( IRout(11 downto 8) ) is
-						when "0000" =>
+						when "0000" => -- NO OPERATION
 						when "0001" =>
 							next_state <= SH;
 						when "0010" =>
@@ -129,23 +132,28 @@ begin
 					case( IRout(9 downto 8) ) is
 					
 						when "00" =>
-							ALUOut_on_dataBus <= '1';
-							RFDWrite <= '1';
+							next_state <= S5;
+							--ALUOut_on_dataBus <= '1';
+							--RFDWrite <= '1';
 						when "01" =>
-							ALUOut_on_dataBus <= '1';
-							RFDWrite <= '1';
+							next_state <= S5;
+							--ALUOut_on_dataBus <= '1';
+							--RFDWrite <= '1';
 						when "10" =>
+							next_state <= SW;
 							Address_on_dataBus <= '1';
 							PCplusI <= '1';
 							RFDWrite <= '1';
 						when "11" =>
+							next_state <= SW;
 							RD_on_addressUnit_RSide <= '1';
 							RplusI <= '1';
 							EnablePC <= '1';
 						when others =>
-					
+							next_state <= SW;			
 					end case ;
 				else
+					next_state <= S35;
 					case( IRout(15 downto 12) ) is
 						when "0001" => -- MOVE
 							ALUOut_on_dataBus <= '1';
@@ -188,20 +196,128 @@ begin
 							ALUOut_on_dataBus <= '1';
 							RFDWrite <= '1';
 						when "1110" => -- CMP 
-
-						when others =>
-					
+						when others =>			
 					end case ;
 				end if;
 				shadow <= '0';
 				data <= IRout(7 downto 0);
+			when S35 =>
+				next_state <= S4;
+				writeMem <= '0';
+				IRLoad <= '0';
+				CSet <= '0';
+				ZSet <= '0';
+				SRload <= '0';
+				WPAdd <= '0';
+				WPLoad <= '0';
+				EnablePC <= '1';
+				PCplus1 <= '0';
+				PCplusI <= '0';
+				Rplus0 <= '0';
+				RplusI <= '0';
+				Address_on_dataBus <= '0';
+				ALUOut_on_dataBus <= '0';
+				RS_on_addressUnit_RSide <= '0';
+				RD_on_addressUnit_RSide <= '0';
+				RFSWrite <= '0';
+				RFDWrite <= '0';
+				readMem <= '0';
+				ResetPC <= '0';
+				IRLoad <= '0';
+				CReset <= '0';
+				ZReset <= '0';
+				WPReset <= '0';
+			when S4 => 
+				next_state <= SW;
+				case( IRout(7 downto 4) ) is
+					when "0001" => -- MOVE
+						ALUOut_on_dataBus <= '1';
+						RFDWrite <= '1';
+					when "0010" => -- LOAD
+						RS_on_addressUnit_RSide <= '1';
+						RFDWrite <= '1';
+						readMem <= '1';
+						Rplus0 <= '1';
+					when "0011" => -- STORE
+						ALUOut_on_dataBus <= '1';
+						RD_on_addressUnit_RSide <= '1';
+						writeMem <= '1';
+						Rplus0 <= '1';
+					when "0100" => -- IN PORT
 
-
-			when S4 =>
+					when "0101" => -- OUT PORT
+					when "0110" => -- AND
+						ALUOut_on_dataBus <= '1';
+						RFDWrite <= '1';
+					when "0111" =>
+						ALUOut_on_dataBus <= '1';
+						RFDWrite <= '1';
+					when "1000" =>
+						ALUOut_on_dataBus <= '1';
+						RFDWrite <= '1';
+					when "1001" =>
+						ALUOut_on_dataBus <= '1';
+						RFDWrite <= '1';
+					when "1010" =>
+						ALUOut_on_dataBus <= '1';
+						RFDWrite <= '1';
+					when "1011" =>
+						ALUOut_on_dataBus <= '1';
+						RFDWrite <= '1';
+					when "1100" =>
+						ALUOut_on_dataBus <= '1';
+						RFDWrite <= '1';
+					when "1101" =>
+						ALUOut_on_dataBus <= '1';
+						RFDWrite <= '1';
+					when "1110" => -- CMP 
+					when others =>			
+				end case ;
 			when S5 =>
-			when S6 =>
-			when SH =>
+				next_state <= SW;
+				if IRout(15 downto 12) = "1111" then
+					case( IRout(9 downto 8) ) is	
+						when "00" =>
+							ALUOut_on_dataBus <= '1';
+							RFDWrite <= '1';
+						when "01" =>
+							ALUOut_on_dataBus <= '1';
+							RFDWrite <= '1';
+						when others =>
+					end case ;
+				end if;
+			when SW =>
+				next_state <= S1;
+				
+				PCplus1 <= '1';
+				readMem <= '1';
+				
+				writeMem <= '0';
+				ResetPC <= '0';
+				IRLoad <= '0';
+				CReset <= '0';
+				ZReset <= '0';
+				CSet <= '0';
+				ZSet <= '0';
+				SRload <= '0';
+				WPReset <= '0';
+				WPAdd <= '0';
+				WPLoad <= '0';
+				EnablePC <= '1';
+				PCplus1 <= '0';
+				PCplusI <= '0';
+				Rplus0 <= '0';
+				RplusI <= '0';
+				shadow <= '0';
+				Address_on_dataBus <= '0';
+				ALUOut_on_dataBus <= '0';
+				RS_on_addressUnit_RSide <= '0';
+				RD_on_addressUnit_RSide <= '0';
+				RFSWrite <= '0';
+				RFDWrite <= '0';
 
+			when SH =>
+				next_state <= SH;
 		end case;
 	end process;
 end architecture ; -- RTL
